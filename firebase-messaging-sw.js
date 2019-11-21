5931eb38463c2f85381facfe50aacf91194e277f
githubprabin143 <prabinkarki643@gmail.com>
2019-11-21 20:32:11 +0530

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+const firebaseConfig = {
+    apiKey: "AIzaSyA-JgzRMNqZh8e1puDEvUPlwBG3idMIs24",
+    authDomain: "pntest-d32ab.firebaseapp.com",
+    databaseURL: "https://pntest-d32ab.firebaseio.com",
+    projectId: "pntest-d32ab",
+    storageBucket: "pntest-d32ab.appspot.com",
+    messagingSenderId: "244254845020",
+    appId: "1:244254845020:web:13a79099e09663a60e9e99",
+    measurementId: "G-2TLX2E8HVT"
+  };
 
-firebase.initializeApp({
-    messagingSenderId: "244254845020"
-});
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
