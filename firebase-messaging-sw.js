cb03e945796b8197410d1eddac94b09fbac2c254
githubprabin143 <prabinkarki643@gmail.com>
2019-11-21 12:19:51 +0530

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: "244254845020"
+});
+
+const messaging = firebase.messaging();
